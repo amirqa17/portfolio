438ca7e7933c354a2c5d48baa6c2ca22210a9eef
amirqa17 <ibraimov.amir1996@gmail.com>
2023-09-18 03:21:32 +0900

    Your commit message here

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -11,30 +11,25 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <div className=" mx-auto max-w-screen-2xl px-4 md:px-8 mb-8">
-  
-  <footer className=" py-4">
-    <hr></hr>
-      <div className="container mx-auto px-4 mt-8">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-2 md:mb-0">
-            <p className="text-xl text-black font-semibold mb-2">Amir Ibraimov</p>
-            <p className="text-black">Front-End Developer | Actively Seeking Opportunities in Japan</p>
-          </div>
-          <div>
-          <p className="text-black">
+    <div className="mx-auto max-w-screen-2xl px-4 md:px-8 mb-8">
 
-         <a href="mailto:Ibraimov.amir1996@gmail.com">Ibraimov.amir1996@gmail.com</a>
-          </p>
-          <p className="text-black">+81 070 3137 1220</p>
-      
-            {/* Add more contact information if needed */}
+      <footer className="py-4">
+        <hr className="border-gray-300 mb-4"></hr>
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="mb-4 md:mb-0">
+              <p className="text-xl text-gray-600 font-semibold mb-2">Amir Ibraimov</p>
+              <p className="text-black">Front-End Developer</p>
+            </div>
+            <div>
+              <p className="text-black mb-2">
+                <a href="mailto:Ibraimov.amir1996@gmail.com">Ibraimov.amir1996@gmail.com</a>
+              </p>
+              <p className="text-black">+81 070 3137 1220</p>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
-
-   
+      </footer>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home = () => {
       <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
        
-        <p className="mb-4 text-center text-lg text-indigo-800 sm:text-xl md:mb-8">
+        <p className="mb-4 text-center text-lg text-gray-500 sm:text-xl md:mb-8">
   Front-End Developer | Actively Seeking Opportunities in Japan 
 </p>
 
@@ -25,7 +25,7 @@ const Home = () => {
 
             
             <div className="relative flex flex-col items-center p-4 sm:max-w-xl">
-            <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl"></h1>
+
   <p className="mb-4 text-center text-lg text-indigo-200 sm:text-xl md:mb-8">Hi, I am Amir, a Front End Developer currently based in Japan with a passion for crafting visually stunning and user-friendly web experiences</p>
 
 
