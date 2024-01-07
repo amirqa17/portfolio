d08e8dbf40c2391a577504330613f2cc5e725bad
amirqa17 <49957940+amirqa17@users.noreply.github.com>
2024-01-08 08:31:36 +0900

    Improving navbar by adding reusable elements

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,13 +2,7 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { TbAlignBoxRightBottomFilled } from "react-icons/tb";
 import Link from "next/link";
-import {
-  BsLinkedin,
-  BsTelegram,
-  BsWhatsapp,
-  BsLine,
-  BsGithub,
-} from "react-icons/bs";
+import { BsLinkedin, BsTelegram, BsWhatsapp, BsGithub } from "react-icons/bs";
 
 const Navbar: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -17,191 +11,137 @@ const Navbar: React.FC = () => {
   const handleMobileMenuToggle = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+  const menuElements = [
+    {
+      href: "/",
+      title: "Home",
+    },
+    {
+      href: "/about",
+      title: "About Me",
+    },
+  ];
+  const menuItems = menuElements.map((item) => (
+    <Link
+      href={item.href}
+      passHref
+      key={item.href}
+      className={`mx-auto text-base font-semibold lg:text-lg hover:bg-gray-200 hover:text-black transition duration-300 px-2 py-1 rounded ${
+        isLinkActive(item.href) ? "bg-gray-300 underline text-black" : ""
+      }`}
+    >
+      {item.title}
+    </Link>
+  ));
+  const socialIcons = [
+    {
+      href: "https://www.linkedin.com/in/amiribraimov/",
+      icon: <BsLinkedin size={34} />,
+      text: "LinkedIn",
+      color: "text-blue-700 hover:text-blue-600",
+    },
+    {
+      href: "https://t.me/Amiracle17",
+      icon: <BsTelegram size={34} />,
+      text: "Telegram",
+      color: "text-blue-500 hover:text-blue-400",
+    },
+    {
+      href: "https://wa.me/+8107031371220",
+      icon: <BsWhatsapp size={34} />,
+      text: "WhatsApp",
+      color: "text-green-500 hover:text-green-400",
+    },
+    {
+      href: "https://github.com/amirqa17",
+      icon: <BsGithub size={34} />,
+      text: "GitHub",
+      color: "text-gray-600 hover:text-gray-800",
+    },
+  ];
+  
+  const socialLinks = socialIcons.map((icon) => (
+    <a
+              href={icon.href}
+              key={icon.href}
+              className={`${icon.color} mx-auto transition-transform transform hover:scale-110`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {/* Show text on mobile, and icon on desktop */}
+              <span className="md:hidden">{icon.text}</span>
+              <span className="hidden md:inline">{icon.icon}</span>
+            </a>
+  ));
+  
 
   return (
-    
-      <nav className="bg-gray-100 text-black p-4 px-4 md:px-8 mb-8">
-        <div className="container mx-auto flex items-center justify-between flex-wrap">
-          <Link
-            href="/"
-            passHref
-            className="inline-flex items-center gap-2.5 text-xl font-bold text-black md:text-3xl"
-            aria-label="logo"
-          >
-            <TbAlignBoxRightBottomFilled className="text-4xl" />/
-            Amir Ibraimov
-          </Link>
-
-         <button
-            className="md:hidden pl-2" // Adjusted padding for better positioning
-            onClick={handleMobileMenuToggle}
-            aria-label="Toggle Menu"
-          >
-            {isMobileMenuOpen ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </button>
-
-          <div
-            className= "hidden md:flex space-x-12 "
-          
-          >
-            <Link
-              href="/"
-              passHref
-              className={`text-base font-semibold lg:text-lg hover:bg-gray-200 hover:text-black transition duration-300 px-2 py-1 rounded ${
-                isLinkActive("/") ? "bg-gray-300 underline text-black" : ""
-              }`}
+    <nav className="bg-gray-100 text-black p-4 px-4 md:px-8 mb-8">
+      <div className="container mx-auto flex items-center justify-between flex-wrap">
+        {/*Logo*/}
+        <Link
+          href="/"
+          passHref
+          className="inline-flex items-center gap-2.5 text-xl font-bold text-black md:text-3xl"
+          aria-label="logo"
+        >
+          <TbAlignBoxRightBottomFilled className="text-4xl" />/ Amir Ibraimov
+        </Link>
+        {/*Mobile menu toggle button*/}
+        <button
+          className="md:hidden pl-2"
+          onClick={handleMobileMenuToggle}
+          aria-label="Toggle Menu"
+        >
+          {isMobileMenuOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              Home
-            </Link>
-            <Link
-              href="/about"
-              passHref
-              className={`text-base font-semibold lg:text-lg hover:bg-gray-200 hover:text-black transition duration-300 px-2 py-1 rounded ${
-                isLinkActive("/about") ? "bg-gray-300 underline text-black" : ""
-              }`}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              About Me
-            </Link>
-          </div>
-
-          <div className="hidden md:flex space-x-4">
-            <a
-              href="https://www.linkedin.com/in/amiribraimov/"
-              className="text-blue-800 hover:text-blue-900 transition-transform transform hover:scale-110"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BsLinkedin size={34} />
-            </a>
-            <a
-              href="https://t.me/Amiracle17"
-              className="text-blue-500 hover:text-blue-600 transition-transform transform hover:scale-110"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BsTelegram size={34} />
-            </a>
-            <a
-              href="https://wa.me/+8107031371220"
-              className="text-green-600 hover:text-green-700 transition-transform transform hover:scale-110"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BsWhatsapp size={34} />
-            </a>
-            <a
-              href="https://github.com/amirqa17"
-              className="text-gray-700 hover:text-black transition-transform transform hover:scale-110"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BsGithub size={34} />
-            </a>
-          </div>
-        </div>
-
-        {isMobileMenuOpen && (
-          <div className="md:hidden mt-4">
-            <ul className="space-y-4">
-              <li>
-                <Link
-                  href="/"
-                  passHref
-                  className={`block text-base font-semibold text-black lg:text-lg ${
-                    isLinkActive("/") ? "text-gray-800" : ""
-                  }`}
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/about"
-                  passHref
-                  className={`block text-base font-semibold text-black lg:text-lg ${
-                    isLinkActive("/about") ? "text-gray-800" : ""
-                  }`}
-                >
-                  About Me 
-                </Link>
-              </li>
-              <li>
-                  <a
-                    href="https://www.linkedin.com/in/amiribraimov/"
-                    className="block text-base font-semibold text-blue-800 hover:text-blue-900 mt-4"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    LinkedIn
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://t.me/Amiracle17"
-                    className="block text-base font-semibold text-blue-500 hover:text-blue-600 mt-4"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Telegram
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://wa.me/+8107031371220"
-                    className="block text-base font-semibold text-green-600 hover:text-green-700 mt-4"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    WhatsApp
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/amirqa17"
-                    className="block text-base font-semibold text-gray-700 hover:text-black mt-4"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    GitHub
-                  </a>
-                </li>
-
-              </ul>
-            </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
           )}
-        </nav>
-   
-    );
-  };
+        </button>
+        {/*Menu items desktop*/}
+        <div className="hidden md:flex space-x-12">{menuItems}</div>
+
+        {/*Social links*/}
+        <div className="hidden md:flex space-x-4">{socialLinks}</div>
+      </div>
+      {/*Menu items mobile*/}
+      {isMobileMenuOpen && (
+        <div className="md:hidden mt-4 flex flex-col space-y-4">
+          {menuItems}
+           {/*Social links mobile*/}
+          {socialLinks}
+        </div>
+        
+      )}
+    </nav>
+  );
+};
 
 export default Navbar;
